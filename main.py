ea2c35bc55dae4ce458a316df118223aa621ad52
cashtann <switalamaciej5@gmail.com>
2025-05-21 21:41:38 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,38 @@
+import random
+
+board = []
+row = []
+board_size = 10
+for i in range(board_size):
+    row.append("X")
+for i in range(board_size):
+    board.append(row)
+
+
+def drawBoard():
+    size_mult = 1 if board_size < 10 else 2
+    canvas = size_mult * " "
+
+    for j in range(board_size):
+        canvas += "|" + chr(65+j)
+
+    canvas += "|\n"
+    for i in range(board_size):
+        canvas += str(i+1) + " |" if (i+1) < 10 else str(i+1) + "|"
+
+        for j in range(board_size):
+            canvas += board[j][i] + "|"
+        canvas += "\n"
+
+
+            
+    canvas += "|\n"
+
+
+
+    print(canvas)
+
+drawBoard()
+
+
+
